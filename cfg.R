cfc2124eb2c1e31ed1aea1754c335802395e12d1
Ryan Hawkins <hawkinsrt@vcu.edu>
2018-04-26 15:02:32 -0400

    Removed csv file paths.  No longer needed now that we are pulling straight from the database

--- cfg.R
+++ cfg.R
@@ -1,35 +1,4 @@
-# ----------------------------------------------
-# FILE LOCATIONS
-# ----------------------------------------------
-
-print("Loading Constants")
-
-
-# File/Dir Names
-
-INPUT_FILE_DIR   = "C:/Users/user/Desktop/SchoolWork/Semester 2/Cases/Data/Raw Data/"
-OUTPUT_FILE_DIR  = "C:/Users/user/Desktop/SchoolWork/Semester 2/Cases/"
-OUTPUT_FILENAME  = "Cases - Final Project - Group 6.pdf"
-FULL_OUTPUT_PATH = paste(OUTPUT_FILE_DIR, OUTPUT_FILENAME, sep="")
-
-
-# Moved file directories to top for easier line of sight for users
-
-SCANS_FN         = "factalertscans.csv"
-SHIPMENTS_FN     = "factalertshipments.csv"
-STORE_FN         = "dimstore.csv"
-DATE_FN          = "dimdate.csv"
-
-
-# Paths
-
-SCANS_PATH       = paste(INPUT_FILE_DIR,  SCANS_FN,        sep="")
-SHIPMENTS_PATH   = paste(INPUT_FILE_DIR,  SHIPMENTS_FN,    sep="")
-STORE_PATH       = paste(INPUT_FILE_DIR,  STORE_FN,        sep="")
-DATE_PATH        = paste(INPUT_FILE_DIR,  DATE_FN,        sep="")
-
-
-# Other Settings
+# Settings
 
 FORECAST_OFFSET = 2    # Number of days out to forecast
 PROCESSING_DATE = as.Date("2018-01-23")
